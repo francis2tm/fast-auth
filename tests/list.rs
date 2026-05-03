5ddb8ae5d3b55bbeddbf0a67a8d297fac6c6b393
francis2tm <franciscomelot@hotmail.com>
2026-05-03 23:34:35 +0200

    fix: tests

--- tests/list.rs
+++ tests/list.rs
@@ -1,9 +1,13 @@
 use fast_auth::handlers::API_KEYS_PATH;
-use test_utils::http::test_http_context_spawn;
+use test_utils::app::{TestHttpContext, test_app_spawn};
+
+async fn context_spawn() -> TestHttpContext {
+    test_app_spawn(Default::default()).await.context().await
+}
 
 common::list_endpoint_test_suite!(
     api_keys_list,
-    test_http_context_spawn,
+    context_spawn,
     |context| format!("{}{}", context.base_url, API_KEYS_PATH),
     "created_at"
 );
